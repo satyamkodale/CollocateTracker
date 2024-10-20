a690c84bac335c236a4bedf2c6e42792d653c795
satyamkodale <satyamkodale@gmail.com>
2024-10-20 12:46:50 +0530

    Initial Commit . Created new project and setup db

---
+++ src/main/java/com/be/CollocateTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollocateTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollocateTrackerApplication.class, args);
+	}
+
+}
